3ce869d84da0e100b091f0d484d681a1cd1a6ca7
alessandrostorchi92 <storchi.alle@gmail.com>
2023-08-21 23:06:26 +0200

    svolgimento bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+    name: "AppHero",
+    data() {
+    },
+
+    methods: {
+
+    }
+}
+
+</script>
+
+<template>
+    <div class="jumbotron"></div>
+</template> 
+
+<style>
+
+.jumbotron {
+    background-image: url();
+    width: 100%;
+}
+
+</style>
