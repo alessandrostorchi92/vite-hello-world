6d902009ab71e2b84f30c0065a3523e493d36bfd
alessandrostorchi92 <storchi.alle@gmail.com>
2023-08-21 21:41:22 +0200

    Esercizio completato

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    name: "AppTitle",
+    data() {
+
+    },
+
+    methods: {
+
+    }
+}
+
+</script>
+
+<template>
+
+<h1>La mia prima app con Vite!</h1>
+
+</template> 
